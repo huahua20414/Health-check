25bb1333aca678c91f04fc0ab9f34d9ce43d99a1
fujiayuan <fujiayuan@baimaohui.net>
2026-06-28 16:00:04 +0800

    Add late schedule time options

--- frontend/src/react/views/AdminViews.jsx
+++ frontend/src/react/views/AdminViews.jsx
@@ -13,7 +13,7 @@ const scheduleWeekdays = [
   { value: 0, label: '周日' },
 ]
 
-const scheduleTimeOptions = ['08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00'].map((start) => {
+const scheduleTimeOptions = ['08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30'].map((start) => {
   const [hour, minute] = start.split(':').map(Number)
   const date = new Date(2000, 0, 1, hour, minute + 30)
   const end = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`
